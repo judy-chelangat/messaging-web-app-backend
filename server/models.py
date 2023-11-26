94f642e7344e67f8e731baa0f64df98c48cb9bfc
judy-chelangat <judysigilai75@gmail.com>
2023-11-26 10:55:11 +0300

    created table message

--- server/models.py
+++ server/models.py
@@ -8,3 +8,14 @@ metadata = MetaData(naming_convention={
 })
 
 db = SQLAlchemy(metadata=metadata)
+
+class Message(db.Model, SerializerMixin):
+    __tablename__ = 'messages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    body=db.Column(db.String)
+    username=db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)  # Use db.DateTime and default value
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    
